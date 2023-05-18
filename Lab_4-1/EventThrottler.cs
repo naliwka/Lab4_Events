c17649c948c5cd4b20306ed011f794b623bcc921
naliwka <avierina.nataliia@gmail.com>
2023-05-18 10:24:39 +0300

    Lab_4-1

---
+++ Lab_4-1/EventThrottler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab_4_1
+{
+    public class EventThrottler
+    {
+        private Dictionary<string, List<Action<EventData>>> eventHandlers;
+        private Dictionary<string, DateTime> lastEventTimes;
+        private int throttleIntervalMs;
+
+        public EventThrottler(int throttleIntervalMs)
+        {
+            this.throttleIntervalMs = throttleIntervalMs;
+            eventHandlers = new Dictionary<string, List<Action<EventData>>>();
+            lastEventTimes = new Dictionary<string, DateTime>();
+        }
+
+        public void Subscribe(string eventName, Action<EventData> eventHandler)
+        {
+            if (!eventHandlers.ContainsKey(eventName))
+            {
+                eventHandlers[eventName] = new List<Action<EventData>>();
+                lastEventTimes[eventName] = DateTime.MinValue;
+            }
+            eventHandlers[eventName].Add(eventHandler);
+        }
+
+        public void Unsubscribe(string eventName, Action<EventData> eventHandler)
+        {
+            if (eventHandlers.ContainsKey(eventName))
+            {
+                eventHandlers[eventName].Remove(eventHandler);
+            }
+            if (eventHandlers[eventName].Count == 0)
+            {
+                eventHandlers.Remove(eventName);
+                lastEventTimes.Remove(eventName);
+            }
+        }
+        public void Publish(string eventName, EventData eventData)
+        {
+            if (!eventHandlers.ContainsKey(eventName))
+                return;
+
+            DateTime currentTime = DateTime.Now;
+            DateTime lastEventTime = lastEventTimes[eventName];
+
+            if ((currentTime - lastEventTime).TotalMilliseconds < throttleIntervalMs)
+                return;
+
+            lastEventTimes[eventName] = currentTime;
+            foreach (var handler in eventHandlers[eventName])
+            {
+                ThreadPool.QueueUserWorkItem(_ =>
+                {
+                    handler(eventData);
+                });
+            }
+        }
+    }
+}
--- Lab_4-1/Program.cs
+++ Lab_4-1/Program.cs
@@ -3,13 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Lab_4_1
 {
     internal class Program
     {
+        private static void eventHandler(EventData eventData)
+        {
+            Console.WriteLine($"Unsubscribed from Event1: {eventData.EventName} - {eventData.TimeStamp}");
+        }
         static void Main(string[] args)
         {
+            EventThrottler eventBus = new EventThrottler(1000); // Інтервал обмеження: 1 секунда
+
+            
+            eventBus.Subscribe("Event1", eventData =>
+            {
+                Console.WriteLine($"Event1 handled: {eventData.EventName} - {eventData.TimeStamp}");
+            });
+
+            
+            eventBus.Subscribe("Event2", eventData =>
+            {
+                Console.WriteLine($"Event2 handled: {eventData.EventName} - {eventData.TimeStamp}");
+            });
+            
+            for (int i = 0; i < 10; i++)
+            {
+                EventData eventData = new EventData
+                {
+                    EventName = i % 2 == 0 ? "Event1" : "Event2",
+                    TimeStamp = DateTime.Now
+                };
+
+                eventBus.Publish(eventData.EventName, eventData);
+
+                Thread.Sleep(500);
+
+                eventBus.Unsubscribe("Event1", eventHandler);
+
+                
+                EventData eventDataAfterUnsubscribe = new EventData
+                {
+                    EventName = "Event1",
+                    TimeStamp = DateTime.Now
+                };
+                eventBus.Publish(eventDataAfterUnsubscribe.EventName, eventDataAfterUnsubscribe);
+
+                Console.ReadLine();
+            }
         }
     }
 }
